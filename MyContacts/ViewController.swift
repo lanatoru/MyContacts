995d84fb3ff3bc0b36308e3fdb71df06786ffe06
Yuliya <yuliya.masalkina@outlook.com>
2025-08-24 21:46:48 +0500

    Initial Commit

---
+++ MyContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyContacts
+//
+//  Created by Yuliya Masalkina on 24.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
